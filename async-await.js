b4f3f5c2ac5f1e5e335ef21b0caf25bac12d2782
David Ko <david@f1v.co>
2020-03-06 10:20:44 -0500

    add more challenge problems

---
+++ async-await.js
@@ -0,0 +1,22 @@
+// 1.
+// create a promise that passes value 10 on resolve and 50 on reject
+
+const myPromise = new Promise(function(resolve, reject) {
+  // fill in
+});
+
+// 2.
+// modify the promise so that it has a 50% chance of rejecting/resolving
+// use Math.random() -> returns a number between 0.00000 and 1.00000
+
+const myPromise = new Promise(function(resolve, reject) {
+  // fill in
+});
+
+// 3.
+// on resolve, console log the result
+// on reject, console error the result
+
+myPromise // chain
+  .then(() => {})
+  .catch(() => {});
