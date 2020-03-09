ec3adb4222a4e59d61c5fd4f749e4129d4468d7c
David Ko <david@f1v.co>
2020-03-09 17:53:10 -0400

    add more problems

---
+++ convert-barchart-to-0s-and-1s.js
@@ -0,0 +1,15 @@
+// have a barchart represented as an array of numbers
+// the index is the x value and value is the height
+// [3, 1, 2, 4]
+//              _       [
+//  _          | |       [0, 0, 0, 1],
+// | |         | |       [1, 0, 0, 1]
+// | |      _  | |       [1, 0, 1, 1]
+// | |  _  | | | |       [1, 1, 1, 1]
+// | | | | | | | | ====>             ]
+
+const input = [3, 1, 2, 4];
+
+const transform = nums => {};
+
+const output = transform(input);
